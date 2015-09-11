3227b0d3cf304daa8a14ab644f89d3552247ec8c
erickr007 <erickr007@gmail.com>
2015-09-11 12:51:22 -0700

    reverting test changes

--- IdeaBag.iOS_Test2/Main.cs
+++ IdeaBag.iOS_Test2/Main.cs
@@ -9,7 +9,7 @@ namespace IdeaBag.Client.iOS
 		{
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
-			UIApplication.Main (args, null, "AppDelegats");
+			UIApplication.Main (args, null, "AppDelegate");
 		}
 	}
 }
